3d3908fd0ff5a8511e2114a67660de8d6599bb01
Gsantos90 <gabrielssantos90@yahoo.com.br>
2025-10-24 19:38:15 -0300

    Ajuste Relatório

--- src/main/java/org/serratec/Cleantech/repository/PedidoRepository.java
+++ src/main/java/org/serratec/Cleantech/repository/PedidoRepository.java
@@ -1,7 +1,15 @@
 package org.serratec.Cleantech.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.serratec.Cleantech.Domain.Pedido;
 
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    @Query("SELECT p.status, COUNT(p) FROM Pedido p GROUP BY p.status")
+    List<Object[]> countPedidosByStatus();
+
+    @Query("SELECT p.status, COUNT(p) FROM Pedido p WHERE p.data BETWEEN :start AND :end GROUP BY p.status")
+    List<Object[]> countPedidosByStatusBetween(LocalDateTime start, LocalDateTime end);
 }
