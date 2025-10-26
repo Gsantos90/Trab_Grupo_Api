56df5972cbf70b1cdfce9acf50d1e68a43cd7153
Clau <suticlaudiane@gmail.com>
2025-10-25 22:09:00 -0300

    Atualização final

--- src/main/java/org/serratec/Cleantech/config/SwaggerConfig.java
+++ src/main/java/org/serratec/Cleantech/config/SwaggerConfig.java
@@ -1,9 +1,7 @@
 package org.serratec.Cleantech.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,18 +9,11 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
 
     @Bean
-    public OpenAPI cleantechOpenAPI() {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Cleantech API")
-                        .description("API do projeto Cleantech - Sistema de gestão sustentável")
                         .version("1.0.0")
-                        .contact(new Contact()
-                                .name("Equipe Cleantech")
-                                .email("cleantech@serratec.org")
-                                .url("https://www.serratec.org"))
-                        .license(new License()
-                                .name("Apache 2.0")
-                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")));
+                        .description("Documentação da API Cleantech"));
     }
 }
